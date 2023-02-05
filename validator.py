b9b915acff17f91475998c2df3f447a048a786f6
IBRAHIM <123403608+Hema675675@users.noreply.github.com>
2023-02-05 20:05:35 +0300

    Delete validator.py

--- validator.py
+++
@@ -1,8 +0,0 @@
-# Example of a custom module to be imported
-
-import re
-
-
-def validate_email_iy(email_iy):
-    if len(email_iy) > 7:
-        return bool(re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email_iy))
